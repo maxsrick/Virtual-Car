66f77658aa12e90ed20d40cdc671b5e8230526e6
Maxwell Rick <bluemax8411@gmail.com>
2020-07-12 15:25:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,15 +9,15 @@ const double estimate_weight = 73.0;
 
 double down_ramp(car* Car, track* Track)
 {
-	double angle = Track.get_inclination();
-	double distance = Track.get_length();
-	Car.set_inclination(angle);
-	double net_force = Car.get_net_force(); //maybe will need to change
-	double a = net_force / Car.get_mass();
+	double angle = Track->get_inclination();
+	double distance = Track->get_length();
+	Car->set_inclination(angle);
+	double net_force = Car->get_net_force(); //maybe will need to change
+	double a = net_force / Car->get_mass();
 	double vf = sqrt(2*a*distance);
 	double t = sqrt(2*distance/a);
-	Car.travel(distance);
-	Car.set_speed(vf);
+	Car->travel(distance);
+	Car->set_speed(vf);
 	return t;
 }
 
