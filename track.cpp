7a16a4e16aaac5f1c0e7559865bd7133ad6ffd6f
Maxwell Rick <bluemax8411@gmail.com>
2020-09-14 11:21:01 -0500

    Update track.cpp

--- track.cpp
+++ track.cpp
@@ -12,24 +12,32 @@ track::track()
     m_total_length = 0.0;
     m_num_segs = 0;
 
-    // straight downward ramp
-    segment downhill_ramp;
-    downhill_ramp.inclination = -M_PI/4; //-1.0 * 0.4612 * M_PI / 180.0; // real value
-    downhill_ramp.length = 584.3;
-    add_segment(&downhill_ramp);
-    
-    // rough code for additional segments
-//    segment top_turn;
-//    top_turn.turnangle = M_PI;
-//    top_turn.length = 58.996;
-//    add_segment(&top_turn);
-    
-//    segment uphill_ramp(downhill_ramp);
-//    uphill_ramp.inclination *= -1.0;
-//    add_segment(&uphill_ramp);
+    //new updated track
+    segment top_turn;
+    top_turn.turnangle = M_PI;
+    top_turn.length = 147.836;
 
-//    segment bottom_turn(top_turn);
-//    add_segment(&bottom_turn);
+    segment long_downhill;
+    long_downhill.length = 580.762;
+    long_downhill.inclination = -0.00998 * M_PI / 180.0;
+
+    segment bot_turn;
+    bot_turn.turnangle = M_PI;
+    bot_turn.length = 90.785;
+
+    segment first_uphill;
+    first_uphill.length = 291.049;
+    first_uphill.inclination = 0;
+
+    segment second_uphill;
+    second_uphill.length = 306.149;
+    second_uphill.inclination = 0.0165 * M_PI / 180.0;
+
+    add_segment(&top_turn);
+    add_segment(&long_downhill);
+    add_segment(&bot_turn);
+    add_segment(&first_uphill);
+    add_segment(&second_uphill);
 }
 
   //accessor methods
